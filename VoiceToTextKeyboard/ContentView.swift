d69abac3794fb839a4794e33ac3f4b532a4839f3
vikashjaingarg04 <ankitjainkherijalab@gmail.com>
2025-08-24 14:30:00 +0530

    Initial Commit

---
+++ VoiceToTextKeyboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoiceToTextKeyboard
+//
+//  Created by Ankit Jain on 24/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoiceToTextKeyboard/VoiceToTextKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceToTextKeyboardApp.swift
+//  VoiceToTextKeyboard
+//
+//  Created by Ankit Jain on 24/08/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceToTextKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
